b84413cf8e59dcb064c09c7d8fece579c2abfbb3
Michael Blandford <mdblandford@gmail.com>
2018-06-14 21:19:01 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,6 @@ def valid_move?(board,index)
   if position_taken?(board,index)
     false
   elsif index < 0 || index > 8
-    false 
-  elsif index == nil
     false
   else
     true
@@ -12,7 +10,7 @@ end
 
 def position_taken?(board,index)
   if board[index] == "X" || board[index] =="O"
-    true 
+    true
   else board[index] ==" " || board[index] == "" || board[index] == nil
     false
   end
